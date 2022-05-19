9ae18d10fe5494b083e75c112ca687817e36aa6e
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-19 09:29:22 +0900

    Standard Styleへ

--- 01.fizzbuzz/main.js
+++ 01.fizzbuzz/main.js
@@ -1,5 +1,5 @@
 // 判定する最大値の設定
-const MAX_NUM = 20;
+const MAX_NUM = 20
 
 const main = () => {
   for (let i = 1; i <= MAX_NUM; i++) {
@@ -9,14 +9,14 @@ const main = () => {
 
 // FizzBuzz変換関数
 const fizzbuzzChanger = (num) => {
-  let answer = '';
+  let answer = ''
 
-  num % 3 || (answer += 'Fizz'); // 3の倍数ならFizzをanswerに結合
-  num % 5 || (answer += 'Buzz'); // 5の倍数ならBuzzをanswerに結合
-  answer == '' && (answer += num); // answerが空のままならnumをそのまま返答
+  num % 3 || (answer += 'Fizz') // 3の倍数ならFizzをanswerに結合
+  num % 5 || (answer += 'Buzz') // 5の倍数ならBuzzをanswerに結合
+  answer == '' && (answer += num) // answerが空のままならnumをそのまま返答
 
-  return answer;
+  return answer
 }
 
 // main関数を実行
-main();
+main()
