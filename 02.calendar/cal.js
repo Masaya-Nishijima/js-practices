f36a1c8de06857593962f7df7d5db9b8c5c52dec
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-24 10:30:07 +0900

    リファクタリング
    関数名変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 const main = () => {
-  const options = cutInput(process.argv.slice(2))
+  const options = parseArgs(process.argv.slice(2))
   console.log(`      ${options.m}月 ${options.y}`)
   const date = new Date(Date.UTC(options.y, --options.m))
   process.stdout.write('日 月 火 水 木 金 土\n')
   printCalMain(date)
 }
 
-const cutInput = (inputOptions) => {
+const parseArgs = (inputOptions) => {
   const today = new Date()
   const defaultOption = {
     default: {
