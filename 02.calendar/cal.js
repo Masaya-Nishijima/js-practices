aa2a99306a032de1a17861a0159f119072af00ef
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-26 10:37:56 +0900

    リファクタリング

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,7 +2,7 @@
 
 const main = () => {
   const options = parseArgs(process.argv.slice(2))
-  const date = new Date(Date.UTC(options.y, options.m - 1))
+  const date = new Date(Date.UTC(options.y, options.m - 1, 1))
 
   console.log(`      ${date.getMonth() + 1}月 ${date.getFullYear()}`)
   process.stdout.write('日 月 火 水 木 金 土\n')
